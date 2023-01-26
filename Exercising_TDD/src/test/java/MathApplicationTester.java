e460b8ee860165ffda146cbbc5c96630f3acf9b3
정찬수 <chansoo@jeongchansuui-MacBookAir.local>
2023-01-26 20:27:23 +0900

    add Mockito test example

---
+++ Exercising_TDD/src/test/java/MathApplicationTester.java
@@ -0,0 +1,48 @@
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@RunWith(MockitoJUnitRunner.class)
+public class MathApplicationTester {
+
+    @InjectMocks
+    MathApplication mathApplication = new MathApplication();
+
+    @Mock
+    CalculatorService calculatorService;
+
+    @Test
+    public void add_Test()
+    {
+
+        when(calculatorService.add(10.0,10.0)).thenReturn(20.0);
+        Assert.assertEquals(mathApplication.add(10.0,10.0),20.0,1.0);
+        verify(calculatorService).add(10.0,10.0);
+    }
+    @Test
+    public void subtract_Test()
+    {
+
+        when(calculatorService.subtract(10.0,10.0)).thenReturn(0.0);
+        Assert.assertEquals(mathApplication.subtract(10.0,10.0),0.0,0.0);
+        verify(calculatorService).subtract(10.0,10.0);
+    }
+    @Test
+    public void multiply_Test()
+    {
+        when(calculatorService.multiply(10.0,10.0)).thenReturn(100.0);
+        Assert.assertEquals(mathApplication.multiply(10.0,10.0),100.0,0.0);
+    }
+    @Test
+    public void divide_Test()
+    {
+        when(calculatorService.divide(10.0,10.0)).thenReturn(1.0);
+        Assert.assertEquals(mathApplication.divide(10.0,10.0),1.0,0.0);
+    }
+}
